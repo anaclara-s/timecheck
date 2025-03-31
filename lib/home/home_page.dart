913ebdacaaadc74f9da249bb7043e5f4c6c1d59c
Ana <anaclarasantos.amorin@gmail.com>
2025-03-31 17:28:17 -0300

    mudança no design da lista

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:timecheck/shared/widgets/custom_header_container.dart';
 
 import '../shared/widgets/custom_appbar.dart';
-import '../shared/widgets/custom_bottom_navigator_bar.dart';
 import '../shared/widgets/custom_text_button.dart';
 import 'controller/home_controller.dart';
 import 'widget/clock_widget.dart';
@@ -34,41 +32,78 @@ class HomePageView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppbarWidget(
-        text: 'Marcações de ponto',
+        text: 'Time Clock',
       ),
-      body: Column(
-        children: [
-          CustomHeaderContainerWidget(
-            child: Column(
-              children: [
-                GreetingWidget(userName: state.widget.userName),
-                SizedBox(height: 50),
-                ClockWidget(),
-              ],
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxHeight: constraints.maxHeight,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 80, 184, 216),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(200),
+                        bottomRight: Radius.circular(200),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        GreetingWidget(userName: state.widget.userName),
+                        SizedBox(height: 20),
+                        ClockWidget(),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: CustomTextButtonWidget(
+                      key: ValueKey(state.nextRecordType),
+                      onPressed: state.recordTime,
+                      text: state.buttonText,
+                      isLoading: state.isLoading,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      'Últimas marcações',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.5,
+                    child: ListView.builder(
+                      itemCount: state.lastFiveRecords.length,
+                      itemBuilder: (context, index) {
+                        final record = state.lastFiveRecords[index];
+                        return Card(
+                          margin:
+                              EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                          child: ListTile(
+                            title: Text(
+                                '${record.time} - ${record.formattedDate}'),
+                            subtitle:
+                                Text(state.formatRecordType(record.recordType)),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          CustomTextButtonWidget(
-            key: ValueKey(state.nextRecordType),
-            onPressed: state.recordTime,
-            text: state.buttonText,
-            isLoading: state.isLoading,
-          ),
-          SizedBox(height: 40),
-          Text('Últimas marcações'),
-          Expanded(
-            child: ListView.builder(
-              itemCount: state.lastFiveRecords.length,
-              itemBuilder: (context, index) {
-                final record = state.lastFiveRecords[index];
-                return ListTile(
-                  title: Text('${record.time} - ${record.formattedDate}'),
-                  subtitle: Text(state.formatRecordType(record.recordType)),
-                );
-              },
-            ),
-          ),
-          CustomBottomNavigatorBarWidget(),
-        ],
+          );
+        },
       ),
     );
   }
